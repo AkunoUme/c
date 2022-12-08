2e4bde8a280f88839fbe47b6a43784e48af10264
AkunoUme <viky185@yandex.ru>
2022-12-08 16:21:22 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x, y, sum;
+    std::cout << "x и y;" << std::endl;
+    std::cin >> x >> y;
+
+    sum = x + y;
+
+    std::cout << sum;
+    return 0;
+}
